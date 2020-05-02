498ac27e5efa1d4796463ed504bc03bd57bef714
max-huddleston <max.huddleston@coxautoinc.com>
2020-05-02 16:47:12 -0400

    add images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,14 @@ import '../App.css';
 import HeaderLink from '../containers/HeaderLink.js';
 import ProjectContainer from '../containers/ProjectContainer.js';
 import max from '../resources/max.png';
+import ic1 from '../resources/Lexie/1_Invisible_Cities/portfolio_site_invisible_cities_1.png';
 
 function Home() {
     return (
         <div className="App">
+            <head title="Max">
+                <script type="text/javascript" src="https://addevent.com/libs/atc/1.6.1/atc.min.js" async defer></script>
+            </head>
             <header className="App-header">
                 <HeaderLink
                     imgLink="https://www.linkedin.com/in/maxhuddleston"
@@ -42,6 +46,15 @@ function Home() {
                     link=""
                     title="Side Project"
                 />
+                {/*<div title="Add to Calendar" className="addeventatc">
+                    Add to Calendar
+                    <span className="start">05/16/2020 08:00 AM</span>
+                    <span className="end">05/16/2020 10:00 AM</span>
+                    <span className="timezone">America/Los_Angeles</span>
+                    <span className="title">Summary of the event</span>
+                    <span className="description">Description of the event</span>
+                    <span className="location">Location of the event</span>
+                </div>*/}
                 <ProjectContainer
                     link=""
                     title="House Project"
@@ -50,6 +63,8 @@ function Home() {
                     link=""
                     title="Lexie Craig Design"
                 />
+                <img src={ic1} alt="invis"/>
+
             </div>
         </div>
     );
